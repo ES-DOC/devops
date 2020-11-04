8162d7ab351304cca539fc45fb0120e5f4fdd456
Mark A. Greenslade <mark@casperlabs.io>
2020-11-04 14:54:06 +0100

    1. Set pyenv local when installing app.

--- opal/setup_apps.sh
+++ opal/setup_apps.sh
@@ -1,2 +1,8 @@
 cd $HOME/apps
+
+# ----------------------------------------------------------------
+# APP 1: pyessv-ws
+# ----------------------------------------------------------------
 git clone https://github.com/ES-DOC/pyessv-ws.git ./test_ws_pyessv
+cd ./test_ws_pyessv
+pyenv local 3.9.0
