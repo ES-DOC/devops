a051251fc62e1191bbaf9775e599363a0df7c28f
Mark A. Greenslade <mark@casperlabs.io>
2020-11-04 21:28:29 +0100

    1. Script suffix fix.

--- opal/setup.sh
+++ opal/setup.sh
@@ -1,6 +1,6 @@
 export HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $HERE/setup_sys.sh
-source $HERE/setup_dirs.sh
+source $HERE/setup_env.sh
 source $HERE/setup_libs.sh
 source $HERE/setup_archives.sh
-source $HERE/setup_vars.sh
+source $HERE/setup_apps.sh
