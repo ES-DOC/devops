acd6351d8cfa96bc93b6604891089114f75f767b
Mark A. Greenslade <mark@casperlabs.io>
2020-11-10 11:59:30 +0100

    1. Removed superfluos code.

--- opal/sync.sh
+++ opal/sync.sh
@@ -20,40 +20,6 @@ declare -a _LIBS=(
 )
 
 #######################################
-# Syncs an application.
-# Globals:
-#   HOME - current user's home directory.
-# Arguments:
-#   Application opal name.
-#   Application github repo.
-#######################################
-function sync_app() {
-    log "... "$1
-
-    if [[ -d $HOME/apps/$1 ]]; then
-        cd $HOME/apps/$1
-        git pull > /dev/null 2>&1
-    else
-        cd $HOME/apps
-        git clone https://github.com/ES-DOC/$2.git ./$1
-        cd ./$1
-    fi
-}
-
-#######################################
-# Syncs set of applications.
-#######################################
-function sync_apps() {
-    log "syncing apps:"
-
-    if [[ ! -d $HOME/apps ]]; then
-        mkdir $HOME/apps
-    fi    
-
-    sync_app "test_ws_pyessv" "pyessv-ws"
-}
-
-#######################################
 # Syncs an archive repo.
 # Globals:
 #   HOME - current user's home directory.
@@ -124,6 +90,5 @@ function sync_libs() {
 #######################################
 # Entry point.
 #######################################
-sync_apps
 sync_archives
 sync_libs
