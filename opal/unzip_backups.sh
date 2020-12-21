487b77fd002046c8bfa6675ee5f83cb0a37182c4
Mark A. Greenslade <mark@casperlabs.io>
2020-12-21 22:21:41 +0100

    1. Added backups.

--- opal/unzip_backups.sh
+++ opal/unzip_backups.sh
@@ -4,6 +4,7 @@
 #   HOME - current user's home directory.
 #######################################
 function main () {
+    rm -rf $HOME/tmp/*
     cp $OPAL_HOME/backups/* $HOME/tmp
     pushd $HOME/tmp
     gzip -d ./*.sql.gz
