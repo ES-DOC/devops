4cfea16be6dd556d2eeba18c5b4cca6e9f38c236
Mark A. Greenslade <asladeofgreen@pm.me>
2022-06-24 17:31:43 +0200

    1. Switched to 2 step process.

---
+++ setups/unbuntu/errata/sh/init.sh
@@ -0,0 +1,19 @@
+# Main entry point.
+main()
+{
+    installer_log "BEGIN initialisation:"
+
+    installer_log "... step 01: initialising system"
+    source $INSTALLER_HOME/sh/init_step_01.sh
+
+    installer_log "... step 02: initialising services"
+    source $INSTALLER_HOME/sh/init_step_02.sh
+
+    installer_log "... step 03: initialising application stack"
+    source $INSTALLER_HOME/sh/init_step_03.sh
+
+    installer_log "END initialisation" 
+}
+
+# Invoke entry point.
+main
---
+++ setups/unbuntu/errata/sh/setup.sh
@@ -0,0 +1,19 @@
+# Main entry point.
+main()
+{
+    installer_log "BEGIN setup:"
+
+    installer_log "... step 01: setting up postgres"
+    source $INSTALLER_HOME/sh/setup_step_01.sh
+
+    installer_log "... step 02: setting up nginx"
+    source $INSTALLER_HOME/sh/setup_step_02.sh
+
+    installer_log "... step 03: setting up application stack"
+    source $INSTALLER_HOME/sh/setup_step_03.sh
+
+    installer_log "END setup" 
+}
+
+# Invoke entry point.
+main
