a9cdc35773d0d0130eda2a000bf16ac4b52f7264
Mark A. Greenslade <asladeofgreen@pm.me>
2022-06-25 00:01:08 +0200

    1. Factoring out stesp by ambient user account.

--- setups/ubuntu/errata/sh/step_01.sh
+++ setups/ubuntu/errata/sh/step_01.sh
@@ -7,13 +7,13 @@ main()
     installer_log "BEGIN step 1:"
 
     installer_log "...step step 1.1: initialising system"
-    source $INSTALLER_HOME/sh/init_step_01.sh
+    source $INSTALLER_HOME/sh/step_01_01.sh
 
     installer_log "... step 1.2: initialising services"
-    source $INSTALLER_HOME/sh/init_step_02.sh
+    source $INSTALLER_HOME/sh/step_01_02.sh
 
     installer_log "... step 1.3: initialising stack"
-    source $INSTALLER_HOME/sh/init_step_03.sh
+    source $INSTALLER_HOME/sh/step_01_03.sh
 
     installer_log "END step 1"
 }
