bc1d412cc60e64b9b496f9954d9192b63035023b
Mark A. Greenslade <asladeofgreen@pm.me>
2022-06-24 22:20:04 +0200

    1. Silencing erroneous log messages.

--- setups/unbuntu/errata/sh/init_step_01.sh
+++ setups/unbuntu/errata/sh/init_step_01.sh
@@ -5,8 +5,8 @@ source $INSTALLER_HOME/sh/utils.sh
 main()
 {
     # Update OS.
-    apt update
-    apt upgrade
+    apt -qq update
+    apt -qq upgrade
 
     # Install base dependencies.
     _install_dependencies
@@ -16,28 +16,28 @@ main()
 _install_dependencies()
 {
     # Installs the C and C++ compilers, also the make command used to compile the CPython dependencies.
-    apt install dpkg-dev build-essential
+    apt install -qq dpkg-dev build-essential
 
     # Install libraries used on packages which implement OpenSSL.
-    apt install libffi-dev libssl-dev
+    apt install -qq libffi-dev libssl-dev
 
     # Install basic utils.
-    apt install wget curl git gh
+    apt install -qq wget curl git gh
 
     # Installs the headers and static libraries of python 3 that are used to compile and mount the language extensions.
-    apt install python3-dev python3-venv
+    apt install -qq python3-dev python3-venv
 
     # Install base of the most of packages that handle databases.
-    apt install libgdbm-dev
+    apt install -qq libgdbm-dev
 
     # Install dependency of psycopg2 to work with PostgreSQL databases.
-    apt install libpq-dev
+    apt install -qq libpq-dev
 
     # Install libraries used to handle XML on packages like lxml.
-    apt install libxml2-dev libxslt1-dev libxmlsec1-dev
+    apt install -qq libxml2-dev libxslt1-dev libxmlsec1-dev
 
     # Install other.
-    apt install \
+    apt install -qq \
         make \
         zlib1g-dev \
         libbz2-dev \
