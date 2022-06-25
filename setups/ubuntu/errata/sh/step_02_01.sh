5f424bfcb5cdd69a35f554bd3f0e403b256d9b48
Mark A. Greenslade <asladeofgreen@pm.me>
2022-06-25 17:40:08 +0200

    1. pyenv setup.

--- setups/ubuntu/errata/sh/step_02_01.sh
+++ setups/ubuntu/errata/sh/step_02_01.sh
@@ -5,7 +5,6 @@ main()
     if [[ ! -f $HOME/.pyenv/bin/pyenv ]]; then
         curl https://pyenv.run | bash
         cat $INSTALLER_HOME/templates/pyenv.txt >> $HOME/.bashrc
-        exec $SHELL
     fi
 }
 
