47c8bafc1aadf35aec89e65d344651cf4842fe8c
Mark A. Greenslade <asladeofgreen@pm.me>
2022-06-27 20:56:26 +0200

    1. Added db credentials.

--- setups/ubuntu/errata/sh/step_06.sh
+++ setups/ubuntu/errata/sh/step_06.sh
@@ -7,10 +7,19 @@ main()
 {
     log "BEGIN step 6:"
 
+    if [[ ! -d /opt/devops/tmp ]]
+        mkdir /opt/devops/tmp
+    fi
+
+    cat >> /opt/devops/tmp/creds.sql <<- EOM
+ALTER USER esdoc PASSWORD '$ERRATA_DB_PWD';
+EOM
+    sudo -i -u postgres psql -d esdoc_errata -q -f /opt/devops/tmp/creds.sql
+
     # log "... step 6.1: creating DB objects"
-	pushd /opt/esdoc-errata-ws
-	pipenv run python $INSTALLER_HOME/sh/step_06.py
-	popd
+	# pushd /opt/esdoc-errata-ws
+	# pipenv run python $INSTALLER_HOME/sh/step_06.py
+	# popd
 
     log "END step 6"
 }
