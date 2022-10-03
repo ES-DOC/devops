16c1827e66a83ee614f6a7cba52aa1ba85c1acc6
Mark A. Greenslade <asladeofgreen@pm.me>
2022-10-03 15:47:16 +0200

    1. Updated cdf2cim devops setup.

--- installers/ubuntu/shared/sh/constants.sh
+++ installers/ubuntu/shared/sh/constants.sh
@@ -2,4 +2,4 @@
 
 # Python versions.
 declare INSTALLER_PYTHON_2=2.7.18
-declare INSTALLER_PYTHON_3=3.9.0
+declare INSTALLER_PYTHON_3=3.10.7
---
+++ installers/ubuntu/ws-cdf2cim/sh/constants.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Imports.
+source $INSTALLER_SHARED/sh/constants.sh
+
+# Array of managed libraries.
+declare -a INSTALLER_REPOS=(
+    'esdoc-cdf2cim-ws'
+    'esdoc-cdf2cim-archive'
+)
