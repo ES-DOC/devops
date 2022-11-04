cae0fb202311878c275ff702dbc8a9b75b80a282
Mark A. Greenslade <asladeofgreen@pm.me>
2022-11-04 12:48:04 +0100

    1. Added revert step.

---
+++ installers/debian/sh/steps/step_06_revert.sh
@@ -0,0 +1,5 @@
+sudo -i -u postgres dropdb esdoc_db_documentation
+sudo -i -u postgres dropdb esdoc_db_errata
+sudo -i -u postgres dropuser esdoc_dbuser_admin
+sudo -i -u postgres dropuser esdoc_dbuser_documentation
+sudo -i -u postgres dropuser esdoc_dbuser_errata
